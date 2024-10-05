b394147b6b61ce076ae328f72d4dba405bdd74df
Toastx <toastx.sol@gmail.com>
2024-10-05 16:03:41 +0530

    changed port to external

--- src/main.rs
+++ src/main.rs
@@ -126,7 +126,7 @@ async fn main() -> std::io::Result<()> {
             .route("/secrets", web::get().to(get_secrets_handler))
             .route("/delete", web::post().to(delete_file_handler))
     })
-    .bind("127.0.0.1:8080")?
+    .bind("0.0.0.0:8000")?
     .run()
     .await?;
 
